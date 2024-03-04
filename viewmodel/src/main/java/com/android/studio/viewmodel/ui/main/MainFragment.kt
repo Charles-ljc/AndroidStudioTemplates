9efb87729926c455493603f3b0d226d7639092b8
Charles <liujinchaods@gmail.com>
2024-03-04 10:43:48 +0800

    Add Iguana | 2023.2.1(2024-02) templates

--- viewmodel/src/main/java/com/android/studio/viewmodel/ui/main/MainFragment.kt
+++ viewmodel/src/main/java/com/android/studio/viewmodel/ui/main/MainFragment.kt
@@ -1,6 +1,6 @@
 package com.android.studio.viewmodel.ui.main
 
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -14,11 +14,11 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
         // TODO: Use the ViewModel
     }
 
